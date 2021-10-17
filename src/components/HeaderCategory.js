96bba3f60822cdc95a0c5086074070149be0827f
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2021-10-17 18:52:41 +0200

    Composant HeaderCategorie

--- src/components/HeaderCategory.js
+++ src/components/HeaderCategory.js
@@ -1,18 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
 
 export default function Header() {
 
+    const { category } = useParams();
+    const [loadInfo, setLoadInfo] = useState(true);
+    const [categories, setCategories] = useState(null);
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BASE_URL}/api/categories/${ category }`)
+        .then(res => {
+            if(res.data){
+                setCategories(res.data)
+                setLoadInfo(false)
+            }
+            setLoadInfo(false)
+        })
+    }, [loadInfo, category])
+    // console.log("categories", categories);
+
+    if(loadInfo === false){
+        return (
+            <section className="header-categorie">
+                <h2>{ categories.nom }</h2>
+                <div className="image">
+                    <img src={"./img/" + categories.url_categorie + "/" + categories.url_categorie + "1.jpg" } alt="categorie" id="img1"/>
+                    <img src={"./img/" + categories.url_categorie + "/" + categories.url_categorie + "2.jpg" } alt="categorie" id="img2"/>
+                    <img src={"./img/" + categories.url_categorie + "/" + categories.url_categorie + "3.jpg" } alt="categorie" id="img3"/>
+                    <img src={"./img/" + categories.url_categorie + "/" + categories.url_categorie + "4.jpg" } alt="categorie" id="img4"/>
+                    <img src={"./img/" + categories.url_categorie + "/" + categories.url_categorie + "5.jpg" } alt="categorie" id="img5"/>
+                </div>
+            </section>
+        )
+    }
     return (
         <section className="header-categorie">
-            <h2>Manucure</h2>
-            <div className="image">
-                <img src="./img/manucure/manucure1.jpg" alt="" id="img1"/>
-                <img src="./img/manucure/manucure2.jpg" alt="" id="img2"/>
-                <img src="./img/manucure/manucure3.jpg" alt="" id="img3"/>
-                <img src="./img/manucure/manucure4.jpg" alt="" id="img4"/>
-                <img src="./img/manucure/manucure5.jpg" alt="" id="img5"/>
-            </div>
-        </section>
+                <h2>Chargement</h2>
+            </section>
     )
     
 }
