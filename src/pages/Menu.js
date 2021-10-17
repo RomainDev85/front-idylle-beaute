8e63d64852bcbe64cf6192ed60e431ee2d7bec81
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2021-10-18 01:56:13 +0200

    page menu stylisé

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Menu() {
     return (
-        <div>
-            Menu
-        </div>
+        <section className="menu-navigation">
+            <ul>
+                <li><Link><i class="far fa-star"></i>Institut</Link></li>
+                <li><Link><i class="far fa-star"></i>Nos produits</Link></li>
+                <li><Link><i class="far fa-star"></i>Nos prestations</Link></li>
+                <li><Link><i class="far fa-star"></i>Contact</Link></li>
+            </ul>
+        </section>
     )
 }
