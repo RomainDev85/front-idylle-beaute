ffcb5bfe865c0f14b69615756cda8cda44323650
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2021-11-04 10:57:22 +0100

    modification position navbar en mode fixe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,16 +17,30 @@ export default function Navbar() {
     }
 
     return (
-        <nav className={menu ? 'navigation-open' : ''}>
-            <div className="navbar">
-                <div className="entreprise">
-                    <motion.h1 initial={{opacity:0, y: 10}} animate={{opacity:1, y:0, transition: {delay: .2}}}>Idylle Beauté</motion.h1>
-                    <motion.h2 initial={{opacity:0, y: 10}} animate={{opacity:1, y:0, transition: {delay: .4}}}>Estheticienne</motion.h2>
+        <>
+            <nav className={menu ? 'navigation-open' : ''}>
+                <div className="navbar">
+                    <div className="entreprise">
+                        <motion.h1 initial={{opacity:0, y: 10}} animate={{opacity:1, y:0, transition: {delay: .2}}}>Idylle Beauté</motion.h1>
+                        <motion.h2 initial={{opacity:0, y: 10}} animate={{opacity:1, y:0, transition: {delay: .4}}}>Estheticienne</motion.h2>
+                    </div>
+                    <motion.div className={menu ? 'menu-btn open' : 'menu-btn'} onClick={handleClick} initial={{opacity:0}} animate={{opacity:1, transition: {delay: .2}}}>
+                        <div className="menu-btn__burger"></div>
+                    </motion.div>
                 </div>
-                <motion.div className={menu ? 'menu-btn open' : 'menu-btn'} onClick={handleClick} initial={{opacity:0}} animate={{opacity:1, transition: {delay: .2}}}>
-                    <div className="menu-btn__burger"></div>
-                </motion.div>
+                <div class="custom-shape-divider-bottom-1636017220">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                    </svg>
+                </div>
+                <div class="custom-shape-divider-top-1636017339">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                    </svg>
+                </div>
+            </nav>
+            <div className="undernav">
             </div>
-        </nav>
+        </>
     )
 }
